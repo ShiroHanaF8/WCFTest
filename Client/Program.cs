9fb04402edd4fe297e42a053092387c07cedf854
ShiroHanaF8 <53520538+ShiroHanaF8@users.noreply.github.com>
2025-05-06 00:13:47 +0900

    Extract methods to Add KeepAlive

--- Client/Program.cs
+++ Client/Program.cs
@@ -14,6 +14,52 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Thread.Sleep(1000); // Wait for the server to start
+                IServerClass proxy = CreateClientEndpoint();
+
+                Console.WriteLine("Client connected on port 60001");
+                CreateCheckClientEndpoint();
+
+                Console.WriteLine("Client callback started on port 60002");
+                Console.WriteLine("Press 'U' to update the server or 'Enter' to exit.");
+                ConsoleKeyInfo input = default;
+                do
+                {
+                    input = Console.ReadKey();
+                    switch (input.Key)
+                    {
+                        case ConsoleKey.U:
+                            Console.WriteLine($"Server Update {proxy.Update()}");
+                            break;
+                    }
+                } while (input.Key != ConsoleKey.Enter);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Client Error \n{ex.Message}");
+            }
+        }
+
+        private static void CreateCheckClientEndpoint()
+        {
+            IClientClass clientClass = new ClientClass();
+            var clientHost = new ServiceHost(clientClass, new Uri("net.tcp://localhost:60002/Client"));
+            clientHost.AddServiceEndpoint(typeof(IClientClass), new NetTcpBinding()
+            {
+                ReliableSession = new OptionalReliableSession()
+                {
+                    Enabled = true,
+                    InactivityTimeout = new TimeSpan(1, 0, 0),
+                }
+            }, "");
+            clientHost.Open();
+        }
+
+        private static IServerClass CreateClientEndpoint()
+        {
+
             // Create a channel factory for the service
             var clientBinding = new NetTcpBinding()
             {
@@ -25,49 +71,10 @@ namespace Client
             };
             var serverEndpoint = new EndpointAddress("net.tcp://localhost:60001/Server");
 
-            Thread.Sleep(1000); // Wait for the server to start
-
-            using (var channelFactory = new ChannelFactory<IServerClass>(clientBinding, serverEndpoint))
-            {
-                // Create a channel to the service
-                IServerClass proxy = null;
-                try
-                {
-                    proxy = channelFactory.CreateChannel();
-                    Console.WriteLine("Client connected on port 60001");
-                    IClientClass clientClass = new ClientClass();
-                    using (var clientHost = new ServiceHost(clientClass, new Uri("net.tcp://localhost:60002/Client")))
-                    {
-                        clientHost.AddServiceEndpoint(typeof(IClientClass), new NetTcpBinding()
-                        {
-                            ReliableSession = new OptionalReliableSession()
-                            {
-                                Enabled = true,
-                                InactivityTimeout = new TimeSpan(1, 0, 0),
-                            }
-                        }, "");
-                        clientHost.Open();
-
-                        Console.WriteLine("Client callback started on port 60002");
-                        Console.WriteLine("Press 'U' to update the server or 'Enter' to exit.");
-                        ConsoleKeyInfo input = default;
-                        do
-                        {
-                            input = Console.ReadKey();
-                            switch (input.Key)
-                            {
-                                case ConsoleKey.U:
-                                    Console.WriteLine($"Server Update {proxy.Update()}");
-                                    break;
-                            }
-                        } while (input.Key != ConsoleKey.Enter);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Client Error \n{ex.Message}");
-                }
-            }
+            var channelFactory = new ChannelFactory<IServerClass>(clientBinding, serverEndpoint);
+            // Create a channel to the service
+            IServerClass proxy = channelFactory.CreateChannel();
+            return proxy;
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -16,58 +16,67 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            var serverInstance = new ServerClass();
-            ServiceHost serverHost = null;
             try
             {
-                using (serverHost = new ServiceHost(serverInstance, new Uri("net.tcp://localhost:60001/Server")))
+                CreateServerEndpoint();
+                Console.WriteLine("Server started on port 60001");
+                Thread.Sleep(1000); // Wait for the client to start
+
+                IClientClass checkChannel = CreateCHeckClientEndpoint();
+
+                Console.WriteLine("Client callback connected on port 60002");
+                Console.WriteLine("Press 'C' to check the client or 'Enter' to exit.");
+
+                ConsoleKeyInfo input = default;
+                do
                 {
-                    serverHost.AddServiceEndpoint(typeof(IServerClass), new NetTcpBinding()
+                    input = Console.ReadKey();
+                    switch (input.Key)
                     {
-                        ReliableSession = new OptionalReliableSession()
-                        {
-                            Enabled = true,
-                            InactivityTimeout = new TimeSpan(1, 0, 0),
-                        }
-                    }, "");
-
-                    serverHost.Open();
-                    Console.WriteLine("Server started on port 60001");
-                    Thread.Sleep(1000); // Wait for the client to start
-
-                    var serverBinding = new NetTcpBinding()
-                    {
-                        ReliableSession = new OptionalReliableSession()
-                        {
-                            Enabled = true,
-                            InactivityTimeout = new TimeSpan(1, 0, 0),
-                        }
-                    };
-                    var endpoint = new EndpointAddress("net.tcp://localhost:60002/Client");
-
-                    using (var channelFactory = new ChannelFactory<IClientClass>(serverBinding, endpoint))
-                    {
-                        var checkChannel = channelFactory.CreateChannel();
-                        Console.WriteLine("Client callback connected on port 60002");
-                        Console.WriteLine("Press 'C' to check the client or 'Enter' to exit.");
-                        ConsoleKeyInfo input = default;
-                        do
-                        {
-                            input = Console.ReadKey();
-                            switch (input.Key)
-                            {
-                                case ConsoleKey.C:
-                                    Console.WriteLine($"Client Check {checkChannel.CheckClient()}");
-                                    break;
-                            }
-                        } while (input.Key != ConsoleKey.Enter);
+                        case ConsoleKey.C:
+                            Console.WriteLine($"Client Check {checkChannel.CheckClient()}");
+                            break;
                     }
-                }
+                } while (input.Key != ConsoleKey.Enter);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Server Error \n{ex.Message}");
             }
+        }
+
+        private static IClientClass CreateCHeckClientEndpoint()
+        {
+            var serverBinding = new NetTcpBinding()
+            {
+                ReliableSession = new OptionalReliableSession()
+                {
+                    Enabled = true,
+                    InactivityTimeout = new TimeSpan(1, 0, 0),
+                }
+            };
+            var endpoint = new EndpointAddress("net.tcp://localhost:60002/Client");
+
+            var channelFactory = new ChannelFactory<IClientClass>(serverBinding, endpoint);
+            var checkChannel = channelFactory.CreateChannel();
+            return checkChannel;
+        }
+
+        private static void CreateServerEndpoint()
+        {
+            var serverInstance = new ServerClass();
+            ServiceHost serverHost = new ServiceHost(serverInstance, new Uri("net.tcp://localhost:60001/Server"));
+
+            serverHost.AddServiceEndpoint(typeof(IServerClass), new NetTcpBinding()
+            {
+                ReliableSession = new OptionalReliableSession()
+                {
+                    Enabled = true,
+                    InactivityTimeout = new TimeSpan(1, 0, 0),
+                }
+            }, "");
+
+            serverHost.Open();
         }
     }
 }
